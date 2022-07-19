eea4001b6926159c7a50f288f4195ba591809184
Franklin Ndlovu <franklin.ndlovu@gmail.com>
2022-07-19 20:24:24 +0200

    added the emit event in the input component

--- app/main.js
+++ app/main.js
@@ -1,10 +1,30 @@
 const inputComponent = {
-    template: `<input class="input is-small" type="text" />`
-}
+    template: `<input class="input is-small" type="text" :placeholder="placeholder" v-model="noteInput" @keyup.enter="monitorEnterKey" />`,
+    props: ['placeholder'],
+    data() {
+        return {
+            noteInput: '',
+        }
+    },
+    methods: {
+        monitorEnterKey(e) {
+            this.$emit('add-note', { note: this.noteInput, timeStamp: new Date().toLocaleString() });
+            this.noteInput = '';
+        }
+    },
+    emits: ['add-note']
+};
 
 const app = {
     components: {
         'input-component': inputComponent
+    },
+    data() {
+        return {
+            notes: [],
+            timeStamps: [],
+            placeholder: 'Enter a note',
+        }
     }
 };
 
