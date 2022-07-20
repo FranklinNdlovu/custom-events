6570e545acc42067176dc856be74453859e42f80
Franklin Ndlovu <franklin.ndlovu@gmail.com>
2022-07-20 10:00:55 +0200

    added note count component

--- app/main.js
+++ app/main.js
@@ -16,9 +16,24 @@ const inputComponent = {
     emits: ['add-note']
 };
 
+const noteCountComponent = {
+    template: `<div class="note-count">Note count: <strong>{{ noteCount }}</strong></div>`,
+    data() {
+        return {
+            noteCount: 0,
+        };
+    },
+    created(){
+        emitter.on('add-note', () => {
+            this.noteCount++;
+        });
+    }
+};
+
 const app = {
     components: {
-        'input-component': inputComponent
+        'input-component': inputComponent,
+        'note-count-component': noteCountComponent,
     },
     data() {
         return {
@@ -33,7 +48,7 @@ const app = {
             this.timestamps.push(event.timestamp);
         }
     },
-    created(){
+    created() {
         emitter.on('add-note', this.addNote);
     }
 };
